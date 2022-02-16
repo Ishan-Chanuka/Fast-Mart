e392f4a3ac8f9bb5f534e6a5107c6cf52159d364
Thilina-Pathirage <thilinapathirage@gmail.com>
2022-02-16 23:09:56 +0530

    Data binding

---
+++ app/src/main/java/com/example/fastmart/LoginFragment.kt
@@ -0,0 +1,22 @@
+package com.example.fastmart
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.example.fastmart.databinding.FragmentLoginBinding
+
+class LoginFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val binding: FragmentLoginBinding = DataBindingUtil.inflate(
+            inflater, R.layout.fragment_login, container, false
+        )
+        return binding.root
+    }
+}
