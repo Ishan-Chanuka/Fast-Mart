21685d48235ee5b95d55c5ecfa5e0505c87855c3
Thilina-Pathirage <thilinapathirage@gmail.com>
2022-02-19 21:30:35 +0530

    update login screen

--- app/src/main/java/com/example/fastmart/LoginFragment.kt
+++ app/src/main/java/com/example/fastmart/LoginFragment.kt
@@ -1,22 +1,25 @@
 package com.example.fastmart
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
 import com.example.fastmart.databinding.FragmentLoginBinding
+
 
 class LoginFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
         val binding: FragmentLoginBinding = DataBindingUtil.inflate(
             inflater, R.layout.fragment_login, container, false
         )
+        (activity as AppCompatActivity?)!!.supportActionBar!!.hide()
         return binding.root
     }
 }
